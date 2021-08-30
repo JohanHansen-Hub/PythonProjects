a7f24a152769aca3baf14d161cb77d190903e047
JohanHansen-Hub <69933675+JohanHansen-Hub@users.noreply.github.com>
2021-08-30 18:47:35 +0200

    commit

---
+++ Forbrukslån/datavisualisering.py
@@ -0,0 +1,22 @@
+
+
+"""
+Vi ønsker oss en deskriptiv oversikt over datasettet; 
+
+--STOLPE DIAGRAM EN--
+y akse antall personer, x akse de ulike gruppene basert på risikonivå
+
+--STOLPE DIAGRAM TO--
+y akse gjennomsnittlig lånesøknadsbeløp, x akse de ulike gruppene
+
+--STOLPE DIAGRAM TRE--
+y akse antall søknader, x akse hver måned/uke
+
+
+
+Vi kan også skaffe oss en oversikt over hvor kundene har bostedsadresse for å sjekke om det er enkelte fylker som en
+reagerer annerledes, gjerne alderrelatert, yrkesrelatert, og/ellers også kan motta en annen reklameeksponering
+Bruke google maps stat...
+
+
+"""
